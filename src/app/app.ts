329ea567192647998339bf4c3c45f3b6919a3bcb
Kubus <jakub.kangowski7@vp.pl>
2026-02-09 08:37:00 +0100

    case animation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {Tracklist} from './components/tracklist/tracklist';
-import {Cd} from './components/cd/cd';
 import {JewelCase} from './components/jewel-case/jewel-case';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Tracklist, Cd, JewelCase],
+  imports: [RouterOutlet, JewelCase],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
